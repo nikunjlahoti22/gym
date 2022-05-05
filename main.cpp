9004fb38664cd7906a5209f18d0249a7acf5409d
nikunjlahoti22 <73753462+nikunjlahoti22@users.noreply.github.com>
2022-05-05 23:27:17 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int row,col;
+    cin>>row>>col;
+    for(int i=1;i<=row;i++){
+        for(int j=1;j<=col;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
